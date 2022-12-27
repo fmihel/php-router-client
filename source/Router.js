3e8b0c33c934eb610b04b1e0a5dd3bb94c4c645b
fmihel <fmihel76@gmail.com>
2022-12-27 16:34:58 +0300

    stash

--- source/Router.js
+++ source/Router.js
@@ -88,46 +88,4 @@ export default class Router {
                 throw new Error(recvPack.msg);
             });
     }
-/*
-    async send({ to, data = {}, params = {} }) {
-        try {
-            const update = { ...this._params, ...params };
-            const { host, id, ...prms } = update;
-
-            const sendPack = this.do('before', { data, to });
-
-            const response = await fetch(
-                host,
-                {
-                    ...prms,
-                    body: JSON.stringify({ [id]: sendPack }),
-                },
-            );
-
-            const recvPack = await response.json(); // parses JSON response into native JavaScript objects
-
-            if (!('res' in recvPack)) {
-                throw new Error('неизвестный ответ');
-            }
-
-            // eslint-disable-next-line eqeqeq
-            if (recvPack.res == 1) {
-                if (!('data' in recvPack)) {
-                    throw new Error('отсутствует data');
-                }
-                this.do('after', recvPack.param);
-                return recvPack.data;
-            }
-
-            if (!('msg' in recvPack)) {
-                throw new Error('ошибка без описания');
-            }
-
-            throw new Error(recvPack.msg);
-        } catch (e) {
-            console.error(e);
-            throw e;
-        }
-    }
-    */
 }
