983155c5d7780b59c4409a6741dd39ee8498f18d
fmihel <fmihel76@gmail.com>
2023-02-16 16:32:01 +0300

    CORS fix

--- source/Router.js
+++ source/Router.js
@@ -10,17 +10,16 @@ export default class Router {
         this.global = {
             host: './',
             id: 'router',
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'no-cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'include', // include, *same-origin, omit
+            method: 'POST',
+            mode: 'cors', // no-cors, cors, same-origin
+            cache: 'no-cache', // default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, same-origin, omit
             headers: {
-                'Content-Type': 'application/json',
-                // 'Content-Type': 'application/x-www-form-urlencoded',
+                // 'Content-Type': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
             },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *client
-            // body: data // body data type must match "Content-Type" header
+            redirect: 'follow', // manual, follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, client
         };
 
         // this.host = window.location.href;
